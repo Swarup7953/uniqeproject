81103e0e0a5b58a3be21eaaf0511ee1738e839e8
Swarup Das <swarupdas7953@gmail.com>
2021-12-14 00:25:02 +0530

    commit

---
+++ myscript.js
@@ -0,0 +1,19 @@
+{/* <script>
+    var myIndex=0;
+
+    function carousel() {
+        var i;
+        var x = document.getElementsByClassName("mySlides");
+        for (let i = 0; i<x.length; i++){
+            x[i].style.display="none";
+        }
+        myIndex++;
+        if (myIndex>x.legth){
+            myIndex=1
+        } 
+        x[myIndex-1].style.display="block";
+        setTimeout(carousel, 5000);   
+           
+        
+    }
+</script> */}
